c965187e55cba96523bdb6897e7c4285c5bdf5f3
radoxubuntu <trans4mer72@gmail.com>
2021-11-17 18:35:58 +0300

    добавлены новые тесты

--- spec/rl_str_gen_spec.rb
+++ spec/rl_str_gen_spec.rb
@@ -8,9 +8,9 @@ describe "rl_str_gen" do
     end
   end
 
-  it "should not contain latin letters, digits or underlines" do
+  it "should contain only valid symbols" do
     1000.times do
-      expect(rl_str_gen.match? /\w/).to be false
+      expect(rl_str_gen.match? /[^а-яё -,\.:!\?\"\';]/i).to be false
     end
   end
 
@@ -53,6 +53,12 @@ describe "rl_str_gen" do
   end
 
   it "should not allow unwanted symbols inside words" do
-
+    1000.times do
+      expect(rl_str_gen.match? /[а-яё-][^а-яё -]+[а-яё-]/).to be false
+    end
   end
+
+  it "should not allow multiple dashes" do
+  end
+
 end
